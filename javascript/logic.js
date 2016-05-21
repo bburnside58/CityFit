6b16f37cc05e766e551466d54fec1b7c892d2f08
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-21 13:26:12 -0400

    added second map

--- javascript/logic.js
+++ javascript/logic.js
@@ -8,11 +8,14 @@ function initialize() {
 		//specifies zoom levels for the map higher levels will be a higher resoution. 0 is a view of the earth
   		zoom: 7, 
   		//specifies the type of map to display. Road map is normal, 2D. Could switch to 'Hybrid' and have photo with road and city names.
-  		mapTypeId: google.maps.MapTypeId.ROADMAP
+  		mapTypeId: google.maps.MapTypeId.Hybrid
 		};
 
 //creates a new map inside the div element googleMap in html using the paramaters that are passed through mapProp
-var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+var map1=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+//creates a second map from the paramaters passed through mapProp
+var map2 =new google.maps.Map(document.getElementById("googleMap2"), mapProp);
 }
 //adds a DOM listener that will execute the initialize function on window when the page is loaded
 google.maps.event.addDomListener(window, 'load', initialize);
