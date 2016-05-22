4d4151080cae2cc39c09122c0f0a0d081db1c007
Devin <devinwiebelt@gmail.com>
2016-05-21 21:55:54 -0400

    added weather data to html using Weather Underground API

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,35 +1,22 @@
-//Initializes the map with page load
-function initialize() {
+//Weather Underground url link connected
+var weather = new XMLHttpRequest();
+weather.open("GET", "http://api.wunderground.com/api/c45e769049bd3145/conditions/q/FL/Orlando.json", false);
+weather.send(null);
 
-//defines the properties for the map
-	 var mapProp = {
-	 	//center is the (lat, long) that will be the center of the map
-  		center:new google.maps.LatLng(51.508742, -0.120850),
-		//specifies zoom levels for the map higher levels will be a higher resoution. 0 is a view of the earth
-  		zoom: 7, 
-  		//lets users drag the map
-  		draggable:true,
-  		//lets users scroll through the page with out moving the map
-  		disableDefaultUI:false,
-  		//specifies the type of map to display. Road map is normal, 2D. Could switch to 'Hybrid' and have photo with road and city names.
-  		mapTypeId: google.maps.MapTypeId.Hybrid
-  		
-		};
-
-//creates a new map inside the div element googleMap in html using the paramaters that are passed through mapProp
-var map1=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-//creates a second map from the paramaters passed through mapProp
-var map2 =new google.maps.Map(document.getElementById("googleMap2"), mapProp);
-}
+//using JSON document to pull real time weather data from API, in this example temperature and wind factor
+var r = JSON.parse(weather.response);
+var weather = "Current Location: " + r.current_observation.display_location.full + "<br />";
+var temp = r.current_observation.temperature_string + "<br />";
+var feels = r.current_observation.feelslike_string + "<br />"
+var wind = r.current_observation.wind_string + "<br />";
+var condition = r.current_observation.weather + "<br />";
+var humidity = r.current_observation.relative_humidity + "<br />"
 
 
-//adds the marker to the center of the map 
-//NEED TO DEFINE CENTER STILL 
-//var marker=new google.maps.Marker({
-	//position:center,
-//});
-//marker.setMap(map);
-
-//adds a DOM listener that will execute the initialize function on window when the page is loaded
-google.maps.event.addDomListener(window, 'load', initialize);
+//using inner HTML technique to post the live weather data to our site 
+document.getElementById("weather").innerHTML = weather;
+document.getElementById("temp").innerHTML = temp;
+document.getElementById("feels").innerHTML = feels;
+document.getElementById("wind").innerHTML = wind;
+document.getElementById("condition").innerHTML = condition;
+document.getElementById("humidity").innerHTML = humidity;
