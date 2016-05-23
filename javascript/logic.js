df210e5f6f3564c6014e92e474ebc9c134dc408a
Brian Burnside <brianburnside@Brians-MacBook-Air.local>
2016-05-23 19:26:36 -0400

    pushing weather data to html

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 
-
+	//weather underground api url
 	$.ajax({
 	  url : "http://api.wunderground.com/api/77cd5507fb26ca1c/hourly/q/autoip.json",
 	  dataType : "jsonp",
 
 		success : function(parsed_json) {
 
-			for (var i = 0; i < 1; i++){
+			//for loop for how many hours we want, 12 sounds good for this web app
+			for (var i = 0; i < 12; i++){
 
+				//variable to grab each data we want
 				var time = parsed_json['hourly_forecast'][i]['FCTTIME']['civil'];
 			  	var condition = parsed_json['hourly_forecast'][i]['condition'];
 				var temp = parsed_json['hourly_forecast'][i]['temp']['english'];
@@ -16,12 +18,34 @@ $(document).ready(function(){
 				// var icon = parsed_json['hourly_forecast'][i]['icon'];
 				// var iconUrl = parsed_json['hourly_forecast'][i]['icon_url'];
 
+				//making a div for each hour of weather data to push into html later
+				var weatherDiv = $('<div>');
 
-				// alert(icon + iconUrl);
-			  	alert("Current time is " + time);
-			  	alert("conditions are " + condition);
-			  	alert("temperature is " + temp + "F");
-			  	alert("the humidity is " + humidity + "%");
+				//Want to try to work to include the icons here or elsewhere at a later time
+				//here are paragraph tags to push into weatherDiv for each weather data parameter
+				var pTime = $('<p>').text(time);
+				var pCondition = $('<p>').text("Local Conditions: " + condition);
+				var pTemp = $('<p>').text("Temp: " + temp + "F");
+				var pHumidity = $('<p>').text("Humidity: " + humidity + "%");
+
+				//appending relevent weather data to weatherDiv div 
+				weatherDiv.append(pTime)
+				weatherDiv.append(pCondition)
+				weatherDiv.append(pTemp)
+				weatherDiv.append(pHumidity)
+
+				//ADDING A CLASS!! TO NEW WEATHER DIVS!!
+				weatherDiv.addClass('jsWeatherDivs')
+				//adding a class to the paragraph tags just in case need it later in css
+				pTime.addClass('jsWeatherPtags')
+				pCondition.addClass('jsWeatherPtags')
+				pTemp.addClass('jsWeatherPtags')
+				pHumidity.addClass('jsWeatherPtags')
+
+				//pushing to html weatherDataDiv div
+				$('#weatherDataDiv').append(weatherDiv)
+				
+
 			}
 		}
 
