88e740c9dbae2e2a12e992581a6782b12304b5bc
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-24 17:48:55 -0400

    places started, no errors but not working

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,16 +1,19 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
+
+var map;
+var infoWindow;
+var request;
+var service;
+var markers = [];
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
+	var center = new google.maps.LatLng(-34.397, 150.644);
+  	map = new google.maps.Map(document.getElementById('map'), {
+    center: center,//{lat: -34.397, lng: 150.644},
+    zoom: 14
+  	});
 
-  // Try HTML5 geolocation.
+
+  // geolocation for where user is
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
@@ -28,30 +31,74 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-   var request = {
-    location: pos,
-    radius: '500',
-    query: 'restaurant'
-  };
 
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
+ 
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-}
-
+//alerts user when there is an error
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
+
+};
+// this is where it defines the area and what we are searching for
+   request = {
+    location: center,
+    radius: 8047, //meters
+    types: ['cafe'] // the criteria that we are looking for in our places - this will need to be replaced with info from the button
+
+  };
+  console.log(request);
+
+//this shows information at each marker
+  infoWindow = new google.maps.InfoWindow({map: map});
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.nearbySearch(request, callback);
 }
+//this function creates the markers at the locations
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      markers.push(createMarker(results[i]));
+    }
+}
+//puts the maker at each of the places
+function createMaker(place){
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
+	return marker;
+}
+
+
+//clears old markers
+function clearResults (markers){
+		for (var m in markers){
+			markers [m].setMap(null)
+		}
+		markers = []
+}
+//this will select a new group of places if the user drags the map and clicks on a different center
+  google.maps.event.addListener(map, 'rightclick', function(event){
+  	map.setCenter(event.latlng) //resets the map
+  	clearResults(markers) //clears the old makers
+  	//new area is defined
+  	var request = {
+  		location: event.latlng,
+  		radius: 8047, 
+  		types: ['cafe'] // will need to update with the button results
+  	};
+  	service.nearbySearch(request, callback);
+  	console.log("request");
+
+  })
+
+
 $(document).ready(function(){
 
 	//weather underground api url
@@ -102,7 +149,9 @@ $(document).ready(function(){
 
 			}
 		}
-	});
+	})
+})
+	};
 
-});
+
 
