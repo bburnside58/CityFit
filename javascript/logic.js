9a01d4b9c1501b03c7e51653a67745ba7bab05da
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-24 07:00:03 -0400

    added start of places

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,75 +1,108 @@
-//Initializes the map with page load
-function initialize() {
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
 
-//defines the properties for the map
-	 var mapProp = {
-	 	//center is the (lat, long) that will be the center of the map
-  		center:new google.maps.LatLng(51.508742, -0.120850),
-		//specifies zoom levels for the map higher levels will be a higher resoution. 0 is a view of the earth
-  		zoom: 7, 
-  		//lets users drag the map
-  		draggable:true,
-  		//lets users scroll through the page with out moving the map
-  		disableDefaultUI:false,
-  		//specifies the type of map to display. Road map is normal, 2D. Could switch to 'Hybrid' and have photo with road and city names.
-  		mapTypeId: google.maps.MapTypeId.Hybrid
-  		
-		};
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
 
-//creates a new map inside the div element googleMap in html using the paramaters that are passed through mapProp
-var map1=new google.maps.Map(document.getElementById("googleMap"), mapProp);
-		//center:{
-			//lat: 51.508742,
-			//lng: -0.120850
-		//},
-		//zoom:15
-	
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-//creates a second map from the paramaters passed through mapProp
-var map2 =new google.maps.Map(document.getElementById("googleMap2"), mapProp);
-};//{
-		//center:{
-			//lat: 51.508742,
-			//lng: -0.120850
-		//},
-	//};
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+   var request = {
+    location: pos,
+    radius: '500',
+    query: 'restaurant'
+  };
 
-//var marker = new google.maps.Marker({
- 	//position:( new google.maps.LatLng(51.508742, -0.120850),
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
 
- 		//map:map,
- 		//draggable:true
- //});
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+}
 
-//This pulls from the google places library of possible destinations
-var searchBox = new google.maps.places.SearchBox(document.getElementById('mapsearch'));
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+$(document).ready(function(){
 
-//place change event on search box
-google.maps.event.addDomListener(searchBox, 'places_changed', function(){
+	//weather underground api url
+	$.ajax({
+	  url : "http://api.wunderground.com/api/77cd5507fb26ca1c/hourly/q/autoip.json",
+	  dataType : "jsonp",
 
-	var places = searchBox.getPlaces();
-	console.log(searchBox.getPlaces());
+		success : function(data) {
 
-	//bound
-	var bounds = new google.maps.LatLngBounds();
-	var i, place;
+			console.log(data);
+			//for loop for how many hours we want, 12 sounds good for this web app
+			for (var i = 0; i < 12; i++){
 
-	for (i=0; place=places[i]; i++){
-		//console.log(place.geometry);
+				//variable to grab each data we want
+				var time = data['hourly_forecast'][i]['FCTTIME']['civil'];
+			  	var condition = data['hourly_forecast'][i]['condition'];
+				var temp = data['hourly_forecast'][i]['temp']['english'];
+				var humidity = data['hourly_forecast'][i]['humidity'];
+				// var icon = parsed_json['hourly_forecast'][i]['icon'];
+				// var iconUrl = parsed_json['hourly_forecast'][i]['icon_url'];
 
-		bounds.extend(place.geometry.location);
-		//maker.setPosition(place.geometry.location); //sets new marker position
-	}
-//map.fitBounds(bounds); //fit to the bound
-//map.setZoom(15);
+				//making a div for each hour of weather data to push into html later
+				var weatherDiv = $('<div>');
+
+				//Want to try to work to include the icons here or elsewhere at a later time
+				//here are paragraph tags to push into weatherDiv for each weather data parameter
+				var pTime = $('<p>').text(time);
+				var pCondition = $('<p>').text("Local Conditions: " + condition);
+				var pTemp = $('<p>').text("Temp: " + temp + "F");
+				var pHumidity = $('<p>').text("Humidity: " + humidity + "%");
+
+				//appending relevent weather data to weatherDiv div 
+				weatherDiv.append(pTime)
+				weatherDiv.append(pCondition)
+				weatherDiv.append(pTemp)
+				weatherDiv.append(pHumidity)
+
+				//ADDING A CLASS!! TO NEW WEATHER DIVS!!
+				weatherDiv.addClass('jsWeatherDivs')
+				//adding a class to the paragraph tags just in case need it later in css
+				pTime.addClass('jsWeatherPtags')
+				pCondition.addClass('jsWeatherPtags')
+				pTemp.addClass('jsWeatherPtags')
+				pHumidity.addClass('jsWeatherPtags')
+
+				//pushing to html weatherDataDiv div
+				$('#weatherDataDiv').append(weatherDiv)
+
+			}
+		}
+	});
+
 });
 
-//adds the marker to the center of the map 
-//NEED TO DEFINE CENTER STILL 
-//var marker=new google.maps.Marker({
-	//position:center,
-//});
-//marker.setMap(map);
-
-//adds a DOM listener that will execute the initialize function on window when the page is loaded
-google.maps.event.addDomListener(window, 'load', initialize);
