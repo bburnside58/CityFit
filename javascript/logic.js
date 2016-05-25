5021eb4f9061127b247f0c18895853af2d72eb23
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-25 19:24:38 -0400

    simplified js

--- javascript/logic.js
+++ javascript/logic.js
@@ -4,9 +4,10 @@ var infoWindow;
 var request;
 var service;
 var markers = [];
+var center;
 
 function initMap() {
-	var center = new google.maps.LatLng(-34.397, 150.644);
+	center = new google.maps.LatLng(-34.397, 150.644);
   	map = new google.maps.Map(document.getElementById('map'), {
     center: center,//{lat: -34.397, lng: 150.644},
     zoom: 14
@@ -47,8 +48,17 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     location: center,
     radius: 8047, //meters
     types: ['cafe'] // the criteria that we are looking for in our places - this will need to be replaced with info from the button
-
-  };
+//puts the maker at each of the places
+};
+function createMarker(place){ //place object
+	var placeLoc = place.geometry.location;
+	var marker = new google.maps.Marker({
+		map: map,
+		position: place.geometry.location
+	});
+	return marker;
+  }
+  createMarker(request);
   console.log(request);
 
 //this shows information at each marker
@@ -64,15 +74,6 @@ function callback(results, status) {
     for (var i = 0; i < results.length; i++) {
       markers.push(createMarker(results[i]));
     }
-}
-//puts the maker at each of the places
-function createMaker(place){
-	var placeLoc = place.geometry.location;
-	var marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location
-	});
-	return marker;
 }
 
 
@@ -99,58 +100,6 @@ function clearResults (markers){
   });
 
 
-$(document).ready(function(){
-
-	//weather underground api url
-	$.ajax({
-	  url : "http://api.wunderground.com/api/77cd5507fb26ca1c/hourly/q/autoip.json",
-	  dataType : "jsonp",
-
-		success : function(data) {
-
-			console.log(data);
-			//for loop for how many hours we want, 12 sounds good for this web app
-			for (var i = 0; i < 12; i++){
-
-				//variable to grab each data we want
-				var time = data['hourly_forecast'][i]['FCTTIME']['civil'];
-			  	var condition = data['hourly_forecast'][i]['condition'];
-				var temp = data['hourly_forecast'][i]['temp']['english'];
-				var humidity = data['hourly_forecast'][i]['humidity'];
-				// var icon = parsed_json['hourly_forecast'][i]['icon'];
-				// var iconUrl = parsed_json['hourly_forecast'][i]['icon_url'];
-
-				//making a div for each hour of weather data to push into html later
-				var weatherDiv = $('<div>');
-
-				//Want to try to work to include the icons here or elsewhere at a later time
-				//here are paragraph tags to push into weatherDiv for each weather data parameter
-				var pTime = $('<p>').text(time);
-				var pCondition = $('<p>').text("Local Conditions: " + condition);
-				var pTemp = $('<p>').text("Temp: " + temp + "F");
-				var pHumidity = $('<p>').text("Humidity: " + humidity + "%");
-
-				//appending relevent weather data to weatherDiv div 
-				weatherDiv.append(pTime)
-				weatherDiv.append(pCondition)
-				weatherDiv.append(pTemp)
-				weatherDiv.append(pHumidity)
-
-				//ADDING A CLASS!! TO NEW WEATHER DIVS!!
-				weatherDiv.addClass('jsWeatherDivs')
-				//adding a class to the paragraph tags just in case need it later in css
-				pTime.addClass('jsWeatherPtags')
-				pCondition.addClass('jsWeatherPtags')
-				pTemp.addClass('jsWeatherPtags')
-				pHumidity.addClass('jsWeatherPtags')
-
-				//pushing to html weatherDataDiv div
-				$('#weatherDataDiv').append(weatherDiv)
-
-			}
-		}
-	})
-})
 	};
 
 
