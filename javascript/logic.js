23f33a9ec01d2c95e54fa238a7e5d0822122b8cc
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-27 07:19:43 -0400

    code clean up, moved infowindow

--- javascript/logic.js
+++ javascript/logic.js
@@ -11,50 +11,49 @@ window.initMap =function() {
   center = new google.maps.LatLng(-34.397, 150.644);
     map = new google.maps.Map(document.getElementById('map'), {
     center: center,//{lat: -34.397, lng: 150.644},
-    zoom: 13
+    zoom: 15
     });
 
-//NEW
+
 // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  //NEW
    // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
+    map.addListener('bounds_changed', function() {
           searchBox.setBounds(map.getBounds());
-        });
-//NEWER
-var infowindow = new google.maps.InfoWindow();
-//NEW
-var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+          });
+   // pulls from google maps the information and sets info windows
+    var infowindow = new google.maps.InfoWindow();
 
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+        
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
           if (places.length === 0) {
-            return;
+          return;
           }
 
-          // Clear out the old markers.
+      // Clear out the old markers.
           markers.forEach(function(marker) {
             marker.setMap(null);
           });
           markers = [];
-  // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
+    // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
           places.forEach(function(place) {
+            //properties for the icon at each place
             var icon = {
               url: place.icon,
-              size: new google.maps.Size(71, 71),
+              size: new google.maps.Size(25, 25),
               origin: new google.maps.Point(0, 0),
               anchor: new google.maps.Point(17, 34),
               scaledSize: new google.maps.Size(25, 25)
             };
 
-            // Create a marker for each place.
+            // Creates a marker for each place and puts them on the map
             markers.push(new google.maps.Marker({
               map: map,
               icon: icon,
@@ -62,58 +61,57 @@ var markers = [];
               position: place.geometry.location
 
             }));
-           
+           //Click event to show info on markers 
             google.maps.event.addListener(markers, 'click', function() {
-              
-            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-            'Place ID: ' + place.place_id + '<br>' +
-              place.formatted_address);
-            infowindow.open(map, markers);
-  
+              console.log('click event registered');
             service.getDetails(place, function(result, status) {
+            //Information on what goes in the info window
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id + '<br>' +
+              place.formatted_address);
+            
 
-              if (status !== google.maps.places.PlacesServiceStatus.OK) {
-          console.error(status);
-          return;
-        }
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+            console.error(status);
+            return;
+            }
 
-     infowindow.setContent(result.name);
-      infowindow.open(map, markers);
-    });
+        infowindow.setContent(result.name);
+        infowindow.open(map, markers);
+       });
   });
 
-            
-            if (place.geometry.viewport) {
+      
+        if (place.geometry.viewport) {
               // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
+          bounds.union(place.geometry.viewport);
+          } else {
+          bounds.extend(place.geometry.location);
+          }
         });
-      };
+        map.fitBounds(bounds);
+      });
+    };
 
 
 
 
   // geolocation for where user is
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+       if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+           };
 
       //infoWindow.setPosition(pos);
       //infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infowindow, map.getCenter());
-    });
-  } else {
+          map.setCenter(pos);
+          }, function() {
+             handleLocationError(true, infowindow, map.getCenter());
+              });
+          } else {
     // Browser doesn't support Geolocation
-    handleLocationError(false, infowindow, map.getCenter());
+         handleLocationError(false, infowindow, map.getCenter());
   }
 
    
@@ -121,9 +119,9 @@ var markers = [];
 //alerts user when there is an error
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   //infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.PlacesServiceStatusContent(browserHasGeolocation ?
+       'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
 
 }
 
